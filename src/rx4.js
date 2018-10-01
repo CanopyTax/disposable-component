4cc393612258f1495625a27a795b286468dcb927
Landon Young <landon.young@canopytax.com>
2018-10-01 12:03:35 -0600

    Add webpack and allow default import

--- src/rx4.js
+++ src/rx4.js
@@ -1,6 +1,7 @@
 import { Observable } from "rx";
 
 export default function mountComponent(mount, unmount) {
+  console.log('4');
   return Observable.create(observer => {
     mount(
       observer.onNext.bind(observer),
--- src/rx6.js
+++ src/rx6.js
@@ -2,6 +2,7 @@ import { Observable } from "rxjs"
 import { finalize } from "rxjs/operators"
 
 export default function mountDisposableComponent(mount, unmount) {
+  console.log('6');
   return Observable.create( observer => {
     mount(
       observer.next.bind(observer),
